e9d3d97939ad4f3f6009a02137454b58f43eef86
jchumgm <jchajnetaumgm@gmail.com>
2023-11-03 13:18:57 +0100

    player

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,18 @@
+import parisImage from "../assets/paris.jpg";
+
+
 export default function VideoCard(props) {
   return (
-    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+    <div class="card card-compact bg-base-100 shadow-xl pb-4">
       <figure>
-        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+        <img src={parisImage} alt="Shoes" />
       </figure>
       <div class="card-body">
         <h2 class="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
+
         </div>
       </div>
     </div>
--- src/components/VideoGrid.jsx
+++ src/components/VideoGrid.jsx
@@ -2,7 +2,7 @@ import VideoCard from './VideoCard'
 
 export default function VideoGrid({ videos }) {
   return (
-    <div class="grid grid-cols-1 gap-1 md:grid-cols-3">
+    <div class="grid grid-cols-1 justif-center gap-1 md:grid-cols-3">
       <VideoCard />
       <VideoCard />
       <VideoCard />
